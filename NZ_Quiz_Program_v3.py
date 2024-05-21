850bdfe37be318859398590aa5c9831a097b8a45
jeongminkimm <kimj7@middleton.school.nz>
2024-05-21 14:11:25 +1200

    Update NZ_Quiz_Program_v3.py

--- NZ_Quiz_Program_v3.py
+++ NZ_Quiz_Program_v3.py
@@ -22,35 +22,37 @@ def welcome():
     print("******************** QUIZ ABOUT NEW ZEALAND ********************")
     print("********** How well do you know Aotearoa New Zealand? **********")
     print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-
-    name = input("\nPlease enter your name: ")
-    # Error prevention - the user can't enter invalid value
-    # The user's name contains non-alphabet
-    if name.isalpha():
-        return f"\nWelcome to the quiz about New Zealand, {name}!"
-    while name != name.isalpha():
-        name = input("Your name should only contain alphabetic characters: ")
-        if name.isalpha():
-            return f"\nWelcome to the quiz about New Zealand, {name}!"
+    return
         
 
 def age(name):
     """Component 3 - Get the user's age"""
-    age = integer_checker(f"Enter {name}'s age: ")
+    age = integer_checker(f"\nEnter {name}'s age: ")
     while age < 5 or age > 11:
-        print("This quiz is only meant for children from 5 to 11 years of age")
+        print("Sorry, this quiz is only meant for children from 5 to 11 years "
+              "of age")
         break
     while age >= 5 and age < 8:
-        print("")
+        print("Quiz A")
     while age > 7 and age < 12:
-        print("")
+        print("Quiz B")
 
 
 # Main routine
 print(welcome())
+name = input("\nPlease enter your name: ")
+    # Error prevention - the user can't enter invalid value
+    # The user's name contains non-alphabet
+if name.isalpha():
+        print(f"\nWelcome to the quiz about New Zealand, {name}!")
+while name != name.isalpha():
+    name = input("Your name should only contain alphabetic characters: ")
+    if name.isalpha():
+        print(f"\nWelcome to the quiz about New Zealand, {name}!")
 # Component 2 - Give the user rules/instructions for the quiz
 print("\nThese are the instructions to start the quiz:"
       "\n1. This quiz consists of multiple-choice questions about New Zealand"
       "\n2. This quiz is only meant for children from 5 to 11 years of age"
       "\n3. Your answer should only be A, B, C, or D"
       "\n4. Your score will be shown at the end of the quiz")
+age(name)
