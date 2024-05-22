66d8bcb7f64a2f0abc31bdfdd66c309ac092e97d
jeongminkimm <kimj7@middleton.school.nz>
2024-05-23 09:30:16 +1200

    nz_quiz_program

--- NZ_Quiz_Program_v5.py
+++ NZ_Quiz_Program_v5.py
@@ -1,5 +1,5 @@
-"""Basic quiz program about New Zealand - v4
-Quiz A started
+"""Basic quiz program about New Zealand - v5
+Quiz B started
 Created By Jeongmin Kim
 """
 
@@ -41,7 +41,7 @@ def age(name):
     if age >= 5 and age < 8:
         quiz_a(questions_A, choices_A, answers_A) # Quiz A
     elif age > 7 and age < 12:
-        print("Quiz B") # Quiz B
+        quiz_B (questions_B, choices_B, answers_B) # Quiz B
     else:
         print("Sorry, this quiz is only meant for children from 5 to 11 years "
               "of age") # Comment for the user who do not meet the age limit
@@ -73,23 +73,23 @@ answers_A = ["B", "B", "C", "C", "A"]
 # List of valid answers
 valid_answers = ["A", "B", "C", "D"]
 
+
 def quiz_a(questions_A, choices_A, answers_A):
+    """Component 4 - Quiz A started"""
     score = 0
     print("\nWelcome to Quiz A for 5 to 7-year-olds"
           "\nPlease type the answer you think is correct"
           "\nLET'S GET STARTED!\n")
-    count = 0
     for count in range(len(questions_A)):
         print(f"Q{count+1}: {questions_A[count]}") # Print questions
         for choice_A in choices_A[count]: # Print choices
             print(choice_A)
         # Get the user's answer
         answer = input("\nEnter your answer ('A', 'B', 'C', or 'D'): ").upper()
-
-        # Error prevention - the user can't enter invalid value
-        # Answers other than 'A', 'B', 'C', or 'D'
-        while answer not in valid_answers:
-            answer = input("Your answer can only be 'A', 'B', 'C', or 'D': "
+    # Error prevention - the user can't enter invalid value
+    # Answers other than 'A', 'B', 'C', or 'D'
+    while answer not in valid_answers:
+        answer = input("Your answer can only be 'A', 'B', 'C', or 'D': "
                             ).upper()
         if answer == answers_A[count]:
             print("Correct!\n") # Comment for correct answers
@@ -100,7 +100,60 @@ def quiz_a(questions_A, choices_A, answers_A):
     return score
 
 
+# Component 5 - Quiz B started
+# List of questions
+questions_B = [
+    "What is the name of the stretch of water that separates the North and "
+    "South Islands?",
+    "Which New Zealand city houses the Beehive?",
+    "Which town has a giant carrot as a landmark?",
+    "Where is 90 mile beach?",
+    "When was the Treaty of Waitangi signed?"
+]
+
+# List of choices
+choices_B = [
+    ["A) Wellington Strait", "B) Tasman Channel", "C) Cook Strait", 
+    "D) Kaikoura Strait"],
+    ["A) Wellington", "B) Christchurch", "C) Paeroa", "D) Auckland"],
+    ["A) Taihape", "B) Waihi", "C) Paeroa", "D) Ohakune"],
+    ["A) Top of the North Island", "B) Bottom of the South Island",
+     "C) Bottom of the North Island", "D) Top of the South Island"],
+     ["A) 1815", "B) 1840", "C) 1855", "D) 1875"]
+]
+
+# List of answers
+answers_B = ["C", "A", "D", "A", "B"]
+
+
+def quiz_B(questions_B, choices_B, answers_B):
+    """Component 5 - Quiz B started"""
+    score = 0
+    print("\nWelcome to Quiz A for 5 to 7-year-olds"
+          "\nPlease type the answer you think is correct"
+          "\nLET'S GET STARTED!\n")
+    for count in range(len(questions_B)):
+        print(f"Q{count+1}: {questions_B[count]}") # Print questions
+        for choice_B in choices_B[count]:
+            print(choice_B)
+        # Get the user's answer
+        answer = input("\nEnter your answer ('A', 'B', 'C', or 'D'): ").upper()
+    # Error prevention - the user can't enter invalid value
+    # Answers other than 'A', 'B', 'C', or 'D'
+    while answer not in valid_answers:
+        answer = input("Your answer can only be 'A', 'B', 'C', or 'D': "
+                            ).upper()
+    if answer == answers_B[count]:
+        print("Correct!\n") # Comment for correct answers
+        score += 1
+    # Comment for wrong answers
+    else:
+        print(f"Wrong. The correct answer is {answers_B[count]}\n")
+    return score
+
+
 # Main routine
+count = 0
 name_ = welcome()
 if name_.isalpha():
     print(f"\nWelcome to the quiz about New Zealand, {name_}!")
