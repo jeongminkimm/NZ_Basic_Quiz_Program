19cecc79c3f843526361c8ef94a7564ef2add81e
jeongminkimm <kimj7@middleton.school.nz>
2024-05-22 23:21:54 +1200

    Update NZ_Quiz_Program_v4.py

--- NZ_Quiz_Program_v4.py
+++ NZ_Quiz_Program_v4.py
@@ -35,7 +35,7 @@ def age(name):
     """Component 3 - Get the user's age"""
     age = integer_checker(f"\nEnter {name}'s age: ")
     if age >= 5 and age < 8:
-        quiz_a()
+        quiz_a(questions_A, choices_A, answers_A)
     elif age > 7 and age < 12:
         print("Quiz B")
     else:
@@ -44,25 +44,56 @@ def age(name):
     return age
 
 
-def quiz_a():
-    """ Component 4 - Quiz A Started"""
-    count = 0
+# Component 4 - Quiz A started
+# List of questions
+questions_A = [
+    "What is the capital of New Zealand?",
+    "Which city is known as 'The Garden City'?",
+    "Where did L&P soda originally come from?",
+    "In what month is Matariki celebrated?",
+    "What colour is Kakariki?"
+]
+
+# List of choices
+choices_A = [
+    ["A) Auckland", "B) Wellington", "C) Christchurch", "D) Hamilton"],
+    ["A) Wellington", "B) Christchurch", "C) Paeroa", "D) Auckland"],
+    ["A) Putaruru", "B) Waihi", "C) Paeroa", "D) Auckland"],
+    ["A) April", "B) May", "C) May, June, or July", "D) July"],
+    ["A) Green", "B) Blue", "C) Black", "D) Grey"]
+]
+
+# List of answers
+answers_A = ["B", "B", "C", "C", "A"]
+
+# List of valid answers
+valid_answers = ["A", "B", "C", "D"]
+
+def quiz_a(questions_A, choices_A, answers_A):
+    score = 0
     print("\nWelcome to Quiz A for 5 to 7-year-olds"
           "\nPlease type the answer you think is correct"
-          "\nLET'S GET STARTED!")
-    print({Quiz_A_questions})
+          "\nLET'S GET STARTED!\n")
+    count = 0
+    for count in range(len(questions_A)):
+        print(f"Q{count+1}: {questions_A[count]}") # Print questions
+        for choice_A in choices_A[count]: # Print choices
+            print(choice_A)
+        # Get the user's answer
+        answer = input("\nEnter your answer ('A', 'B', 'C', or 'D'): ").upper()
 
-    Quiz_A_questions = [
-        "1. What is the capital of New Zealand?\nA. Auckland\nB. Wellington"
-        "\nC. Christchurch\nD. Hamilton",
-        "2. Which city is known as 'The Garden City'?\nA. Wellington"
-        "\nB. Christchurch\nC. Paeroa\nD. Auckland",
-        "3. Where did L&P soda originally come from?\nA. Putaruru"
-        "\nB. Waihi\nC. Paeroa\nD. Auckland",
-        "4. In what month is Matariki celebrated?\nA. April\nB. May"
-        "\nC. May, June, or July\nD. July",
-        "5. What colour is Kakariki?\nA. Green\nB. Blue\nC. Black\nD.Grey"
-    ]
+        if answer == answers_A[count]:
+            print("Correct!\n")
+            score += 1
+        while answer not in valid_answers:
+            answer = input("Your answer can only be 'A', 'B', 'C', or 'D': "
+                            ).upper()
+            if answer == answers_A[count]:
+                print("Correct!\n")
+                score += 1
+        else:
+            print(f"Wrong. The correct answer is {answers_A[count]}\n")
+    return score
 
 
 # Main routine
@@ -73,7 +104,5 @@ if name_.isalpha():
 print("\nThese are the instructions to start the quiz:"
       "\n1. This quiz consists of multiple-choice questions about New Zealand"
       "\n2. This quiz is only meant for children from 5 to 11 years of age"
-      "\n3. Your answer should only be A, B, C, or D"
-      "\n4. Your score will be shown at the end of the quiz")
+      "\n3. Your score will be shown at the end of the quiz")
 age(name_)
-quiz_a
